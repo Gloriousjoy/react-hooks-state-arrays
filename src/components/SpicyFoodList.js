a6de281aa0178b7f6c08b2de9dd7ae6534d73cf9
Joy <106956767+Gloriousjoy@users.noreply.github.com>
2022-09-26 13:04:29 +0300

    Update SpicyFoodList.js

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -3,6 +3,27 @@ import { spicyFoods, getNewRandomSpicyFood } from "../data";
 
 function SpicyFoodList() {
   const [foods, setFoods] = useState(spicyFoods);
+  const [filterBy, setFilterBy] = useState("All
+  const foodsToDisplay = foods.filter((food) => {
+  if (filterBy === "All") {
+    return true;
+  } else {
+    return food.cuisine === filterBy;
+  }
+});
+ 
+function handleFilterChange(event) {
+  setFilterBy(event.target.value);
+}
+  return (
+  <select name="filter" onChange={handleFilterChange}>
+    <option value="All">All</option>
+    <option value="American">American</option>
+    <option value="Sichuan">Sichuan</option>
+    <option value="Thai">Thai</option>
+    <option value="Mexican">Mexican</option>
+  </select>
+);
 
   function handleAddFood() {
     const newFood = getNewRandomSpicyFood();
